4b99648fa28ba0005bfec229f6993b67dd634dd3
MadMcCrow <noe.perard+git@gmail.com>
2024-03-19 11:02:13 +0100

    godot prebuild for MacOS

--- nix/godot/prebuilt.nix
+++ nix/godot/prebuilt.nix
@@ -5,12 +5,14 @@ pkgs :
 let
 stdenv = pkgs.stdenvNoCC;
 
+godotURL = "https://github.com/godotengine/godot/releases/download";
+
 # default builder
 generic = { version, platform, hash } : stdenv.mkDerivation {
   name = "godot";
   inherit version;
   src = pkgs.fetchzip {
-    url = "https://github.com/godotengine/godot/releases/download/${version}/Godot_v${version}_${platform}.zip";
+    url = "${godotURL}/${version}/Godot_v${version}_${platform}.zip";
     inherit hash;
     };
 
@@ -26,7 +28,7 @@ generic = { version, platform, hash } : stdenv.mkDerivation {
     };
 };
 
-generic-linux = args : (generic args).overrideAttrs {
+genericLinux = args : (generic args).overrideAttrs {
   nativeBuildInputs = [
     pkgs.autoPatchelfHook
   ];
@@ -60,24 +62,35 @@ generic-linux = args : (generic args).overrideAttrs {
     ];
 };
 
+genericDarwin = args : (generic args).overrideAttrs {
+  nativeBuildInputs = [];
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/bin/godot.app
+    mv ./* $out/bin/godot.app
+    chmod 751 $out/bin/godot.app
+    runHook postInstall
+  '';
+};
+
 # TODO :
-export = "https://github.com/godotengine/godot/releases/download/4.2.1-stable/Godot_v4.2.1-stable_export_templates.tpz";
+export = {version} : "${godotURL}/${version}/Godot_v${version}_export_templates.tpz";
 
 # select binary
-in if stdenv.isDarwin then generic {
+in if stdenv.isDarwin then genericDarwin {
   version = "4.1.3-stable";
   platform = "macos.universal";
   hash = "sha256-dwZ6q8bktAYBOqx4k6SuIhq17Qx5clnORjqpXPRdc8Q=";
 } 
 else if (stdenv.isLinux && stdenv.isx86_64) then 
-generic-linux
+genericLinux
 {
   version = "4.2.1-stable";
   platform = "linux.x86_64";
   hash = "sha256-dHlwkS2JHgLiXFPncLbSjZ7JqfcySS6k1pYPOyY59yk=";
 } 
 else if (stdenv.isLinux && stdenv.isAarch64) then 
-generic-linux
+genericLinux
 {
   version = "4.2.1-stable";
   platform = "linux.arm64";
